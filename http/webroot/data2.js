11f711dc19e490f7a189d2e8649135e41844c765
keyou <zhangkui@cvte.cn>
2016-08-11 18:29:06 +0800

    初始化项目

---
+++ http/webroot/data2.js
@@ -0,0 +1,35 @@
+console.log("data.js");
+
+(function() {
+    Index = 0;
+
+    var SimpleListModel = function(items) {
+        this.items = ko.observableArray(items);
+        this.itemToAdd = ko.observable("");
+        this.addItem = function() {
+            if (this.itemToAdd() != "") {
+                this.items.push(this.itemToAdd()); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
+                this.itemToAdd(""); // Clears the text box, because it's bound to the "itemToAdd" observable
+            }
+        }.bind(this); // Ensure that "this" is always this view model
+    };
+    var vm = new SimpleListModel(["begin"]);
+
+    function updateData() {
+        var url = "http://localhost:8888/list-all-log?index=" + Index + "&count=" + 50 + "&callback=?";
+        $.getJSON(url, function(result) {
+            // alert("result: " + result);
+            Index += result.length;
+            $.each(result, function(i, field) {
+                vm.items.push(field);
+            });
+            window.scrollTo(0, document.body.scrollHeight);
+            setTimeout(updateData, 1000);
+        }).error(function(error) {
+            console.log(error);
+            setTimeout(updateData, 1000);
+        });
+    }
+    ko.applyBindings(vm);
+    updateData();
+})();
