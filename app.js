593391f7631d79d9d218769d8f2d24aae2befdde
keyou <zhangkui@cvte.cn>
2016-09-20 17:54:44 +0800

    添加使用参数设置redis连接地址

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+var args = process.argv.slice(2);
+console.log('args: ' + process.argv);
+var ip = args[0] || '127.0.0.1';
+var port = args[1] || '36379';
+console.log('redis: ' + ip + ':' + port);
 var http = require('./http/HttpServer');
-http.start('127.0.0.1', '36379');
+http.start(ip, port);
